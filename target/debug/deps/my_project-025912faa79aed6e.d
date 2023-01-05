8b910c8c706f8591e875d810b7a5f092151d765b
Paul Rijneveld <paulrijneveld@gmail.com>
2023-01-05 07:20:27 +0000

    Initial commit

---
+++ target/debug/deps/my_project-025912faa79aed6e.d
@@ -0,0 +1,5 @@
+/home/runner/AOC22Day15/target/debug/deps/my_project-025912faa79aed6e.rmeta: src/main.rs
+
+/home/runner/AOC22Day15/target/debug/deps/my_project-025912faa79aed6e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/my-project.d
@@ -0,0 +1,1 @@
+/home/runner/AOC22Day15/target/debug/my-project: /home/runner/AOC22Day15/src/main.rs
